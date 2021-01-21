b41ea364cae04db9e9314299c90a66057a627f2b
wangwenbo <wangwenbomx@gmail.com>
2021-01-21 15:46:31 +0800

    Initial Commit

---
+++ GitHubActionsDemoTests/GitHubActionsDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  GitHubActionsDemoTests.swift
+//  GitHubActionsDemoTests
+//
+//  Created by wangwenbo on 2021/1/21.
+//
+
+import XCTest
+@testable import GitHubActionsDemo
+
+class GitHubActionsDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
